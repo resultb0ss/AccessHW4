b29bab7a2a60b0fcb04100eeefd578b1bb5448f9
Admin <reteamboss@gmail.com>
2024-08-26 21:20:59 +0300

    Задание4

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,16 @@
 fun main() {
-    println("Hello World!")
+
+    var array = arrayOf(1,2,3,4,5,6,7,8,9,10,-2,-23,-43,-35,-453,-345)
+    checkNum(array)
+}
+
+fun checkNum (array: Array<Int>){
+
+    fun findNum (num: Int):Boolean = num > 0
+
+    for (num in array){
+        if (findNum(num))
+            println(num)
+    }
+
 }
